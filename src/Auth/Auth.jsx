8aa0f1b0a332e0d09de07b55d1eec280d52e6741
ArpitShukla3 <arpit900shukla@gmail.com>
2024-11-09 17:29:09 +0530

    fix

--- src/Auth/Auth.jsx
+++ src/Auth/Auth.jsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 import { Signup } from "./Signup";
 import { Signin } from "./Signin";
 import Animation from "./Animation";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
   const [signin, setSignin] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken) {
+      navigate("/dashboard");
+    }
+  });
   return (
     <div className=" h-screen md:h-[100vh] bg-gradient-to-br from-black via-gray-800 to-gray-900 flex justify-center items-center flex-col md:flex-row">
       <div className="hidden md:flex w-full sm:w-1/3 md:w-[50vh] justify-center items-center sm:mb-8">
